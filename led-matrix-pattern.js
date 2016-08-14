5dde1d7b73b9d1e988c7eb7fef1d4d0699a7addf
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-08-15 01:10:48 +0200

    Minor improvements displaying patterns on the led matrix

--- led-matrix-pattern.js
+++ led-matrix-pattern.js
@@ -1,9 +1,8 @@
-// For use with 74HC595 chip
-
 var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function() {
+  // For use with 74HC595 chip
   var register = new five.ShiftRegister({
     pins: {
       data: 13,
@@ -12,25 +11,26 @@ board.on("ready", function() {
     }
   });
 
-  // Declare and initialize the array for the columns
-  var columnsPins = [2, 4, 5, 16, 17];
-  var columns = new Array(5);
+  var columns = [
+    new five.Pin(2),
+    new five.Pin(4),
+    new five.Pin(5),
+    new five.Pin(16),
+    new five.Pin(17),
+  ];
 
-  for(var i = 0; i < columns.length; i++) {
-    columns[i] = new five.Pin(columnsPins[i]);
+  function print(pattern) {
+    for(var index = 0; index < pattern.length; index++) {
+      register.send(pattern[index]);
+      columns[index].high();
+      setInterval(function() {}, 2);
+      columns[index].low();
+    }
   }
 
-  var pattern = [8, 18, 16, 18, 8];
-  var index = 0;
-
   this.loop(2, function() {
-    register.send(pattern[index]);
-    columns[index].high();
-    setInterval(function() {}, 2);
-    columns[index].low();
-
-    index++;
-    if (index == pattern.length) index = 0;
+    var pattern = [8, 18, 16, 18, 8];
+    print(pattern);
   });
 
 });
