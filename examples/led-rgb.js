d1e81382a2d5cd183e9e32c2d3d882e4469332de
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-08-19 02:19:14 +0200

    Added new colors

--- examples/led-rgb.js
+++ examples/led-rgb.js
@@ -7,6 +7,10 @@ board.on("ready", function() {
   // Turn it on and set the initial color
   led.on();
   led.color("#FF0000");
-
+  setInterval(function() {}, 2000);
+  led.color("#00FF00");
+  setInterval(function() {}, 2000);
+  led.color("#0000FF");
+  setInterval(function() {}, 2000);
   led.blink(1000);
 });
