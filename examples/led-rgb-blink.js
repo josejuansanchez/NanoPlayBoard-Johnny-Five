54654392cd3e41b66e64c4b3baebf30283ffe0ed
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-08-21 18:15:21 +0200

    Removed code

--- examples/led-rgb-blink.js
+++ examples/led-rgb-blink.js
@@ -3,14 +3,7 @@ var board = new five.Board();
 
 board.on("ready", function() {
   var led = new five.Led.RGB([9,10,11]);
-
-  // Turn it on and set the initial color
   led.on();
-  led.color("#FF0000");
-  setInterval(function() {}, 2000);
-  led.color("#00FF00");
-  setInterval(function() {}, 2000);
   led.color("#0000FF");
-  setInterval(function() {}, 2000);
   led.blink(1000);
 });
