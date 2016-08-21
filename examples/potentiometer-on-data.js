24b5cb8d8e05bcfa047401d13a491f175c6a2faf
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-08-22 01:01:57 +0200

    Minor refactor

--- examples/potentiometer-on-data.js
+++ examples/potentiometer-on-data.js
@@ -1,17 +1,12 @@
-var five = require("johnny-five"),
-  board, potentiometer;
-
-board = new five.Board();
+var five = require("johnny-five");
+var board = new five.Board();
 
 board.on("ready", function() {
-
-  // Create a new `potentiometer` hardware instance.
-  potentiometer = new five.Sensor({
+  var potentiometer = new five.Sensor({
     pin: "A1",
     freq: 250
   });
 
-  // "data" get the current reading from the potentiometer
   potentiometer.on("data", function() {
     console.log(this.value, this.raw);
   });
